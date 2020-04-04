26ce7d97a58ce9931a20e33fed191f6699a2377a
V-Kwong <vincent_kwong578@hotmail.com>
2020-04-03 21:09:43 -0700

    upgraded react navigation to version 5

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,16 +1,24 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React, { Component } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
 import MainTabNavigator from './MainTabNavigator';
 
 
-// const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+const ParentStack = createStackNavigator();
 
-export default createAppContainer(createSwitchNavigator({
-  Splash: SplashScreen,
-  Login: LoginScreen,
-  Main: MainTabNavigator,
-}, {
-  initialRouteName: 'Splash',
-}));
+export default class AppNavigator extends Component {
+  render() {
+    return (  
+      <NavigationContainer>
+        <ParentStack.Navigator>
+          <ParentStack.Screen name="Splash" component={SplashScreen} />
+          <ParentStack.Screen name="Login" component={LoginScreen} />
+          <ParentStack.Screen name="Main" component={MainTabNavigator} />
+        </ParentStack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,47 +1,22 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import React, { Component } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 
-const BottomTabNavigatorConfig = {
-  tabBarOptions: {
-    showLabel: false, // hide labels
-    activeTintColor: '#ffe001', // active icon color
-    style: {
-      backgroundColor: '#313131', // TabBar background
-    }
+const MainTab = createBottomTabNavigator();
+// const SettingsStack = createStackNavigator();
+// const HomeStack = createStackNavigator();
+
+export default class MainTabNavigator extends Component {
+  render() {
+    return (
+      <MainTab.Navigator>
+        <MainTab.Screen name="Home"  component={HomeScreen} />
+        <MainTab.Screen name="Settings" component={SettingsScreen} />
+      </MainTab.Navigator>
+    )
   }
 }
-
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
-
-// HomeStack.navigationOptions = {
-//   tabBarIcon: ({ focused }) => (
-//     <EntypoIcon
-//       focused={focused}
-//       name='home'
-//     />
-//   ),
-// };
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-// SettingsStack.navigationOptions = {
-//   tabBarIcon: ({ focused }) => (
-//     <IoniconsIcon
-//       focused={focused}
-//       name='md-settings'
-//     />
-//   ),
-// };
-
-export default createBottomTabNavigator({
-  HomeStack,
-  SettingsStack,
-}, BottomTabNavigatorConfig);
