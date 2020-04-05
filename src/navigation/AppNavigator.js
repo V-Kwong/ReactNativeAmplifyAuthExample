1a8c0f7eda739d5443be876707caa1badf9a93fb
V-Kwong <vincent_kwong578@hotmail.com>
2020-04-04 21:31:38 -0700

    idk what the fuck im doing

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -10,13 +10,54 @@ import MainTabNavigator from './MainTabNavigator';
 const ParentStack = createStackNavigator();
 
 export default class AppNavigator extends Component {
+
+  state = {
+    isLoading: true,
+    isLoggedIn: false,
+  }
+
+  componentDidMount() {
+    this.checkAuth()
+  }
+
+  updateAuth = (isLoggedIn) => {
+    this.setState({
+      isLoggedIn: isLoggedIn
+    })
+  }
+
+  checkAuth = async () => {
+    setTimeout(() => {  
+      this.setState({ 
+        isLoading: false,
+        isLoggedIn: false 
+      })
+    }, 2500);
+    // try {
+    //   await AmplifyAuth.currentAuthenticatedUser()
+    //   console.log('user is signed in')
+    //   this.setState({ currentView: 'mainNav' })
+    // } catch (err) {
+    //   console.log('user is not signed in')
+    //   this.setState({ currentView: 'auth' })
+    // }
+  }
+
   render() {
+    const { isLoading, isLoggedIn } = this.state
+
+    if (isLoading) {
+      return <SplashScreen/>;
+    }
+
     return (  
       <NavigationContainer>
         <ParentStack.Navigator>
-          <ParentStack.Screen name="Splash" component={SplashScreen} />
-          <ParentStack.Screen name="Login" component={LoginScreen} />
-          <ParentStack.Screen name="Main" component={MainTabNavigator} />
+          {isLoggedIn ? (
+            <ParentStack.Screen name="Main" component={MainTabNavigator} />
+          ) : (
+            <ParentStack.Screen name="Login" component={LoginScreen} />
+          ) }
         </ParentStack.Navigator>
       </NavigationContainer>
     );
--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -3,12 +3,6 @@ import { StyleSheet, View, ActivityIndicator } from 'react-native';
 
 
 export default class SplashScreen extends Component {
-  async componentDidMount() {
-    setTimeout(() => {  
-      this.props.navigation.navigate('Login'); 
-    }, 2500);
-  }
-
   render() {
     return (
       <View style={styles.container}>
