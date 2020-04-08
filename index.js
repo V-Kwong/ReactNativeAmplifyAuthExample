2b76186ffb01eb06d4c5d8a7cb30cc697d66fd38
V-Kwong <vincent_kwong578@hotmail.com>
2020-04-07 21:00:51 -0700

    forgot to add Amplify import

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import Amplify from 'aws-amplify';
+
 import App from './App';
 import {name as appName} from './app.json';
 import awsconfig from './aws-exports';
