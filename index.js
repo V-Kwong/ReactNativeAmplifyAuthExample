cee741b52d563e1392a456d842e0abe0a49ac4e3
V-Kwong <vincent_kwong578@hotmail.com>
2020-04-09 14:22:01 -0700

    don't think we need the commented out part but I'll push just in case

--- index.js
+++ index.js
@@ -5,10 +5,15 @@
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
 import Amplify from 'aws-amplify';
+// import { polyfillGlobal } from 'react-native/Libraries/Utilities/PolyfillFunctions';
 
 import App from './App';
 import {name as appName} from './app.json';
 import awsconfig from './aws-exports';
+
+
+// polyfillGlobal('URLSearchParams', () => undefined);
+// delete global.URLSearchParams;
 
 Amplify.configure(awsconfig);
 AppRegistry.registerComponent(appName, () => App);
